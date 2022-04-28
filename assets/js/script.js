b310b7ae6ed0a4b8ab6445cea02ecd829d397e98
Tommy Lee <t.k.hobbes43@gmail.com>
2022-04-27 22:33:26 -0500

    first attempt at setting up questions as an array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,12 @@
-function startGame() {
+const startButton = document.getElementById('start-btn')
 
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+    console.log('Started');
+    startButton.classList.add('hide')
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
 }
 
 function setNextQuestion() {
@@ -17,3 +24,19 @@ function startTimer() {
 function viewHighscores () {
     
 }
+
+const questions = [
+    {
+        question: 'Which HTML element do we put the Javascript in?',
+        answer: [
+            { text: '<script>', correct: true },
+            { text: '<js>', correct: false },
+            { text: '<javascript>', correct: false },
+            { text: '<scripting>', correct: false },
+        ]
+
+        }
+    }
+
+
+]
