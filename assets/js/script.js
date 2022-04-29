b678c70fbbcf9a7b74c5be95f60097d5cece4ab9
Tommy Lee <t.k.hobbes43@gmail.com>
2022-04-29 15:54:20 -0500

    so far got first questions to load, but need to work on user picking an answer and getting timer to start with event lister of clicking on start button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,17 @@
-var startButton = document.getElementById('start-btn')
+var startButton = document.getElementById('start-btn');
 var intro = document.getElementById("intro-container");
 var questions = document.getElementById("question-container");
-var controls = document.getElementById("final-container");
-var final = document.getElementById("controls");
+var final = document.getElementById("final-container");
+var controls = document.getElementById("controls");
 index = 0;
-var questionNumber
-var elements = document.getElementsByClassName("answer")
-
-startButton.addEventListener('click', startGame)
+var questionNumber = 0;
+var elements = document.getElementsByClassName("answer");
+var timer
 
 const quiz = [
     {
         question: 'Which HTML element do we put the JavaScript in?',
-        choices: ['<script', '<js>', '<javascript>', '<scripting>' ],
+        choices: ['script', 'js', 'javascript', 'scripting'],
         answer: '<script>'
     },
     {
@@ -42,12 +41,13 @@ const quiz = [
     },
 ];
 
+startButton.addEventListener('click', startGame)
+
 function startGame() {
     intro.setAttribute('hidden', true);
     document.getElementById('controls').style.display = "none"
+    document.getElementById('final-container').style.display = "none"
     document.getElementById('questions').style.display = "block"
-    document.getElementById('questions').style.display = "none"
-    questionNumber = 0;
     loadQuestion(questionNumber)
     for (var i = 0; i< elements.length; i++) {
         elements[i].addEventListener("click", answerQuestion);
@@ -55,28 +55,43 @@ function startGame() {
 } 
 
 function loadQuestion (questionNumber) {
-    document.getElementById("question_text").innerHTML = quiz[questionNumber]["question"]
-    document.getElementById("option-01").innerHTML = quiz[question_number]["choices"][0]
-    document.getElementById("option-02").innerHTML = quiz[question_number]["choices"][1]
-    document.getElementById("option-03").innerHTML = quiz[question_number]["choices"][2]
-    document.getElementById("option-04").innerHTML = quiz[question_number]["choices"][3]
+    console.log(quiz[questionNumber].question);
+    console.log(quiz[questionNumber].choices);
+    document.getElementById("question-text").innerHTML = quiz[questionNumber].question
+    document.getElementById("option-01").innerHTML = quiz[questionNumber].choices[0]
+    document.getElementById("option-02").innerHTML = quiz[questionNumber].choices[1]
+    document.getElementById("option-03").innerHTML = quiz[questionNumber].choices[2]
+    document.getElementById("option-04").innerHTML = quiz[questionNumber].choices[3]
+    
 }
 
-// function answerQuestion (event) {
-//     answered = event.target.innerHTML
-//     if(answered == quiz[questionNumber]["answer"]) {
-//         time += 10 
-//     } else {
-//         time -= 15
-//     }
-//     questionNumber += 1
-//     if(questionNumber <= 4 ) {
-//         load_question(questionNumber)
-//     } else {
-//         // load input initials page 
-//         document.getElementById("questions").style.display = "none"
-//         document.getElementById("time").style.display = "none"
-//         document.getElementById("log").style.display = "block"
-//         document.getElementById("highScore").innerHTML = time
-//     }
-// }
+document.getElementById('start-btn').addEventListener("click", function() {
+    timer = setInterval(function function1 () {
+        document.getElementById("time").innerHTML = time + "" + "seconds remaining";
+        time -= 1;
+        if (time <= 0) {
+            clearInterval(time);
+            document.getElementById("time").innerHTML = "Game Over"
+            document.getElementById("questions").style.display = "none"
+        }
+    }, 1000);
+});
+
+function answerQuestion (event) {
+    answered = event.target.innerHTML
+    if(answered == quiz[questionNumber]["answer"]) {
+        time += 10 
+    } else {
+        time -= 15
+    }
+    questionNumber += 1
+    if(questionNumber <= 4 ) {
+        load_question(questionNumber)
+    } else {
+        // load input initials page 
+        document.getElementById("questions").style.display = "none"
+        document.getElementById("time").style.display = "none"
+        document.getElementById("log").style.display = "block"
+        document.getElementById("highScore").innerHTML = time
+    }
+}
